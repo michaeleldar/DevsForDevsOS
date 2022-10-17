0c682b935cd2db43753e03a04931dcc64e19c543
Michael Halpin <halpinm2010@gmail.com>
2022-10-17 19:07:13 +0100

    added noconfirm

--- install_arch_linux.py
+++ install_arch_linux.py
@@ -1,9 +1,9 @@
 from os import system as run
 
 
-run("pacman -Syu")
-run(f"pacman -S xorg")
-run(f"pacman -S gdm")
-run(f"pacman -S cutefish")
+run("pacman -Syu --noconfirm")
+run(f"pacman -S xorg --noconfirm")
+run(f"pacman -S gdm --noconfirm")
+run(f"pacman -S cutefish --noconfirm")
 run("systemctl enable gdm.service")
 run("systemctl start gdm.service")
