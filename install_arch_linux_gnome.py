8dbcdff6337c07d225348d6f2aff2709b5e5977a
Michael Halpin <halpinm2010@gmail.com>
2022-10-17 19:30:55 +0100

    add 2 different DE's

---
+++ install_arch_linux_gnome.py
@@ -0,0 +1,11 @@
+from os import system as run
+
+
+run("pacman -Syu --noconfirm")
+run(f"pacman -S xorg --noconfirm")
+run(f"pacman -S gdm --noconfirm")
+run(f"pacman -S gnome --noconfirm")
+run("systemctl enable gdm.service")
+
+
+run("systemctl start gdm.service")
