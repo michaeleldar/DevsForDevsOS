edf507929c221313978a0280098244c669995520
Michael Halpin <halpinm2010@gmail.com>
2022-10-17 18:41:54 +0100

    fix? #2

--- install_arch_linux.py
+++ install_arch_linux.py
@@ -2,12 +2,12 @@ from os import system as run
 
 
 def install(package):
-    run(f"pacman -s {package}")
+    run(f"pacman {package}")
 
 
 run("pacman -Syu")
-run("pacman -S xorg")
 install("xorg")
 install("gdm")
 install("cutefish")
 run("systemctl enable gdm.service")
+run("systemctl start gdm.service")
