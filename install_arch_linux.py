6b2642935376c51c92a88ac883b535d51678d913
Michael Halpin <halpinm2010@gmail.com>
2022-10-17 18:53:31 +0100

    add "s"

--- install_arch_linux.py
+++ install_arch_linux.py
@@ -2,8 +2,8 @@ from os import system as run
 
 
 run("pacman -Syu")
-run(f"pacman xorg")
-run(f"pacman gdm")
-run(f"pacman cutefish")
+run(f"pacman -s xorg")
+run(f"pacman -s gdm")
+run(f"pacman -s cutefish")
 run("systemctl enable gdm.service")
 run("systemctl start gdm.service")
