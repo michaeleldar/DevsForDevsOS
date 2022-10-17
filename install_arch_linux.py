5d8e6f6d82013d30ef78386e7621906389472af0
Michael Halpin <halpinm2010@gmail.com>
2022-10-17 19:14:46 +0100

    change display manager

--- install_arch_linux.py
+++ install_arch_linux.py
@@ -3,7 +3,9 @@ from os import system as run
 
 run("pacman -Syu --noconfirm")
 run(f"pacman -S xorg --noconfirm")
-run(f"pacman -S gdm --noconfirm")
+run(f"pacman -S sddm --noconfirm")
 run(f"pacman -S cutefish --noconfirm")
-run("systemctl enable gdm.service")
-run("systemctl start gdm.service")
+run("systemctl enable sddm")
+
+
+run("systemctl start sddm")
