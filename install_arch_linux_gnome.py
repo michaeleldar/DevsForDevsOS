0c011dedc8418e0a3f8988f8e6957ebf1c6f8eba
Michael Halpin <halpinm2010@gmail.com>
2022-10-18 07:16:01 +0100

    ready for -2

--- install_arch_linux_gnome.py
+++ install_arch_linux_gnome.py
@@ -10,4 +10,17 @@ run("systemctl enable gdm.service")
 run("pacman -Rs epiphany --noconfirm")
 run("pacman -S firefox --noconfirm")
 
+easy_install_answered_correctly = False
+while easy_install_answered_correctly == False:
+    easy_install = input(
+        "Do you want to use the stable (recommended), beta or commitly update channel? "
+    )
+    if easy_install == "stable" or easy_install == "beta" or easy_install == "commitly":
+        easy_install_answered_correctly = True
+    else:
+        print("Please enter stable, nightly or commitly.")
+
+install_config = open("/etc/devsfordevsupdates.config")
+install_config.write()
+
 run("systemctl start gdm.service")
